92fcb704d84f9880028165e10738f7e419c61af1
Kamil <kamil.p@tutanota.com>
2024-03-05 21:54:31 +0100

    add category design

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
 import "./categoryList.css";
 
 export const CategoryList = () => {
-	return <div className="categoryList">CategoryList</div>;
+	return (
+		<div className="categoryList">
+			<h1 className="categoryList__title">Popular Categories</h1>
+			<div className="categoryList__categoriesContainer">
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+				<Link href="/blog?cat=style" className="category category--style">
+					<Image src="/style.png" alt="" width={32} height={32} className="category__image" />
+					style
+				</Link>
+			</div>
+		</div>
+	);
 };
