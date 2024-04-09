09792e398ba636e2435c52f22a21e2f801f92566
Kamil <kamil.p@tutanota.com>
2024-04-09 21:59:11 +0200

    add extend user prisma schema for role, add callbacks to the auth.ts

--- auth.ts
+++ auth.ts
@@ -23,6 +23,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import prisma from "@/utils/connect";
+import { getUserById } from "@/utils/data/user";
 
 export const {
 	handlers: { GET, POST },
@@ -30,6 +31,30 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role;
+			}
+
+			return session;
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+
+			return token;
+		},
+	},
 	adapter: PrismaAdapter(prisma),
 	session: { strategy: "jwt" },
 	...authConfig,
