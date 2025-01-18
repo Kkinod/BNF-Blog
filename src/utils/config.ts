b4cfc33b82d402a51c0f2bc5288dacc32e2b1178
Kamil <kamil.p@tutanota.com>
2025-01-18 19:00:56 +0100

    fix: update getBaseUrl helper to handle Vercel deployment correctly

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,9 @@
 export const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL; // SSR should use vercel url
-  return `http://localhost:3000`; // dev SSR should use localhost
+  if (typeof window !== "undefined") {
+    // browser should use relative path
+    return "";
+  }
+  
+  // SSR should use vercel url
+  return process.env.NEXTAUTH_URL || "http://localhost:3000";
 }; 
